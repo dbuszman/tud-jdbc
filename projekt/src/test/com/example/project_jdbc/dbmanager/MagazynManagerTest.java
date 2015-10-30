22f473962f908a3be951b3e0d4c194c3178e635d
Dariusz Buszman <dbuszman@hotmail.com>
2015-10-30 13:43:37 +0100

    dodanie klasy do testowania MagazynManager

---
+++ projekt/src/test/com/example/project_jdbc/dbmanager/MagazynManagerTest.java
@@ -0,0 +1,13 @@
+package com.example.project_jdbc.dbmanager;
+
+public class MagazynManagerTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
